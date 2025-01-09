d4612d7121ba8807e9d28a3c1b639aa76b3600dc
Fernando Freitas <126774900+ferr-ffk@users.noreply.github.com>
2025-01-09 11:12:32 -0300

    chore(code): extrair variável de cálculo de direção

--- Assets/Componentes/ComponenteMovimento.cs
+++ Assets/Componentes/ComponenteMovimento.cs
@@ -5,39 +5,41 @@ public class ComponenteMovimento : MonoBehaviour
     [SerializeField, Tooltip("A velocidade do objeto. Deve ser definida.")]
     private float _velocidade;
 
-    [SerializeField, Tooltip("A velocidade de pulo, sendo pulo o movimento em direÁ„o Y+. Deve ser definida.")]
+    [SerializeField, Tooltip("A velocidade de pulo, sendo pulo o movimento em dire√ß√£o Y+. Deve ser definida.")]
     private float _velocidadePulo;
 
-    [SerializeField, Tooltip("A aceleraÁ„o do objeto, por padr„o 1.")]
+    [SerializeField, Tooltip("A acelera√ß√£o do objeto, por padr√£o 1.")]
     private float _aceleracao = 1.0f;
 
     private Vector3 velocidadeAtual = Vector3.zero;
 
     /// <summary>
-    /// Movimenta o objeto passado em uma direÁ„o dada.
+    /// Movimenta o objeto passado em uma dire√ß√£o dada.
     /// </summary>
     /// <param name="objeto">O GameObject a ser movimentado</param>
-    /// <param name="direcao">O Vector3 representando a direÁ„o</param>
+    /// <param name="direcao">O Vector3 representando a dire√ß√£o</param>
     public void Movimentar(GameObject objeto, Vector3 direcao)
     {
-        // Calcula a direÁ„o desejada de movimento
+        // Calcula a dire√ß√£o desejada de movimento
         Vector3 velocidadeAlvo = direcao * _velocidade;
 
-        // Calcula o valor da aceleraÁ„o para ser utilizada no Lerp
+        // Calcula o valor da acelera√ß√£o para ser utilizada no Lerp
         float valorAceleracao = 1 - Mathf.Exp(-_aceleracao * Time.deltaTime);
 
-        // Calcula o Lerp, que È basicamente uma forma de deixar mais suave o movimento com aceleraÁ„o
+        // Calcula o Lerp, que √© basicamente uma forma de deixar mais suave o movimento com acelera√ß√£o
         Vector3 direcaoLerp = Vector3.Lerp(velocidadeAtual, velocidadeAlvo, valorAceleracao);
 
-        // Movimenta o objeto
-        objeto.transform.Translate(direcaoLerp *  _velocidade * Time.deltaTime);
+        Vector3 direcaoCalculada = direcaoLerp *  _velocidade * Time.deltaTime;
 
-        // Atualiza a vari·vel local para o valor mais recente da velocidade do objeto
+        // Movimenta o objeto
+        objeto.transform.Translate(direcaoCalculada);
+
+        // Atualiza a vari√°vel local para o valor mais recente da velocidade do objeto
         velocidadeAtual = velocidadeAlvo;
     }
 
     /// <summary>
-    /// Usa a velocidade de pulo do componente para movimentar o objeto na direÁ„o Y+.
+    /// Usa a velocidade de pulo do componente para movimentar o objeto na dire√ß√£o Y+.
     /// </summary>
     /// <param name="objeto"></param>
     public void Pular(GameObject objeto)
