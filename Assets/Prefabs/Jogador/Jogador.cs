13db8c7f930eb1a2f5911c35611e789cc9462de3
Fernando Freitas <freitax.li@gmail.com>
2025-01-24 23:50:59 -0300

    incluir lógica de corrida automática ao andar por no mínimo meio segundo

--- Assets/Prefabs/Jogador/Jogador.cs
+++ Assets/Prefabs/Jogador/Jogador.cs
@@ -15,6 +15,11 @@ public class Jogador : MonoBehaviour
     [SerializeField]
     private GameObject _pivoCamera;
 
+    [SerializeField]
+    private float _delayCorridaAutomatica = 0.5f;
+
+    private bool correndo = false;
+
     private GameObject _camera;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
@@ -41,6 +46,36 @@ public class Jogador : MonoBehaviour
 
         // Utiliza a rotação que a câmera está apontando, e usa para que o jogador se movimente de acordo com ela
         Quaternion rotacaoCameraApontando = Quaternion.Euler(0, _camera.transform.eulerAngles.y, 0);
+
+        float tempoAndando = _componenteMovimento.tempoAndando;
+        bool haMovimento = direcao.magnitude > 0;
+        bool corridaAutomaticaAtivada = tempoAndando > _delayCorridaAutomatica;
+
+        // Verifica se o jogador está andando e se o tempo de andar é maior que o delay
+        // Ativa a corrida automática ao andar pelo tempo necessário
+        if (haMovimento && corridaAutomaticaAtivada)
+        {
+            correndo = true;
+        }
+
+        // Verifica se o jogador parou de andar e reseta o estado de correr
+        if (_componenteMovimento.tempoAndando == 0)
+        {
+            correndo = false;
+        }
+
+        Debug.Log(correndo);
+
+        // Define a velocidade alvo do jogador de acordo com o estado de correr
+        if (correndo)
+        {
+            // Obtém o multiplicador de correr do componente de movimento e atualiza a velocidade alvo para correr
+            _componenteMovimento.VelocidadeAlvo = _componenteMovimento.MultiplicadorCorrendo * _componenteMovimento.VelocidadeInicial;
+        } else
+        {
+            // Reseta a velocidade alvo para a velocidade inicial, sem o multiplicador de velocidade ao correr
+            _componenteMovimento.VelocidadeAlvo = _componenteMovimento.VelocidadeInicial;
+        }
 
         // Utiliza o método do componente de movimento e movimenta o jogador
         _componenteMovimento.Movimentar(gameObject, direcao, rotacaoCameraApontando);
